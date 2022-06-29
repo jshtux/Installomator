eed6a01785e5873621e8dc597bf15224c7befd90
Joel Hatcher <73445840+jshtux@users.noreply.github.com>
2022-06-29 16:31:22 -0500

    Update gotomeeting.sh
    get appNewVersion added

--- fragments/labels/gotomeeting.sh
+++ fragments/labels/gotomeeting.sh
@@ -3,5 +3,6 @@ gotomeeting)
     name="GoToMeeting"
     type="dmg"
     downloadURL="https://link.gotomeeting.com/latest-dmg"
+    appNewVersion=$(curl -s https://support.goto.com/meeting/help/whats-new-in-gotomeeting | grep "Desktop app" | head -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1 | cut -d , -f 1 | cut -b2-)
     expectedTeamID="GFNFVT632V"
     ;;
