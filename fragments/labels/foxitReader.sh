d7e7d9c8707ffacd2383b4e3f700eee5bfcb1197
Joel Hatcher <73445840+jshtux@users.noreply.github.com>
2024-03-27 15:07:46 -0500

    Update foxitReader.sh

--- fragments/labels/foxitReader.sh
+++ fragments/labels/foxitReader.sh
@@ -2,6 +2,8 @@ FoxitReader)
     name="Foxit PDF Reader"
     type="pkg"
     downloadURL=$(curl -fsIL "https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Mac-OS-X&version=&package_type=&language=English&distID=" | grep location | cut -d \  -f 2)
+    # strip trailing carriage return from download url
+    downloadURL=${downloadURL%$'\r'}
     appNewVersion=$( echo $downloadURL | cut -d / -f 10 )
     expectedTeamID="8GN47HTP75"
     blockingProcesses=( "Foxit PDF Reader" "FoxitPDFReaderUpdateService" )
